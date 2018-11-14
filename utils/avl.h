2cc5d786f2c9af2cc956a7225089b04fb6a0f925
rrsilaya <rrsilaya@gmail.com>
2018-11-14 23:03:49 +0800

    Lint fixes

--- utils/avl.h
+++ utils/avl.h
@@ -20,177 +20,169 @@ WORD* createWord(char *string) {
   return node;
 }
 
-int max(int a, int b){
-  return(a>b?a:b);
+int max(int a, int b) {
+  return a > b ? a : b;
 }
 
 int cmpStr(char *a, char *b) {
   return strcmp(a, b) > 0;
 }
 
-void updateHeight(WORD *root){
-  if(root!=NULL)
-    root->height = max(root->left==NULL?-1:root->left->height,root->right==NULL?-1:root->right->height)+1;
+void updateHeight(WORD *root) {
+  if (root != NULL)
+    root -> height = max(
+      root -> left == NULL
+        ? -1
+        : root -> left -> height,
+      root -> right == NULL
+        ? -1
+        : root -> right -> height
+    ) + 1;
 }
 
-int checkBalance(WORD *root){
-
-  //These integers takes the height of the subtrees
+int checkBalance(WORD *root) {
+  // These integers takes the height of the subtrees
   int leftSubTree; 
   int rightSubTree;
 
-  if(root->left == NULL) leftSubTree = -1;
-  else leftSubTree = root->left->height;
+  if (root -> left == NULL) leftSubTree = -1;
+  else leftSubTree = root -> left -> height;
 
-  if(root->right == NULL) rightSubTree = -1;
-  else rightSubTree = root->right->height;
+  if (root -> right == NULL) rightSubTree = -1;
+  else rightSubTree = root -> right -> height;
 
   // If the difference is positive or negative one, then the root is balanced
-  // Else if the difference is positive two, then the tree is imbalanced at left subtree, else if negative two, then imbalanced at right subtree
+  // Else if the difference is positive two, then the tree is imbalanced at left
+  // subtree, else if negative two, then imbalanced at right subtree
   return leftSubTree - rightSubTree;
-
 }
 
-//leftRotate() does a single left rotation given a root
-void leftRotate(WORD **rootptr){
+void leftRotate(WORD **root) {
+  WORD *base = *root;
+  WORD *temp = base -> right; // Left rotation takes note of the right pointer of the root
 
-  WORD *temp = (*rootptr)->right; //Left rotation takes note of the right pointer of the root
-
-  if(rootptr!=NULL && *rootptr!=NULL && (*rootptr)->right!=NULL){
-
-    //Checks if the rootptr has a parent
-    if((*rootptr)->parent != NULL){
-      if((*rootptr)->parent->left == (*rootptr)){ //This assign root->parent->left/right to temp
-        (*rootptr)->parent->left = temp;
-        (*rootptr) = temp->parent;
+  if (root != NULL && base != NULL && base -> right != NULL) {
+    // Checks if the root has a parent
+    if (base -> parent != NULL) {
+      if (base -> parent -> left == base) {
+        // Assigns root->parent->left/right to temp
+        base -> parent -> left = temp;
+        *root = temp -> parent;
+      } else {
+        base -> parent -> right = temp;
       }
-      else(*rootptr)->parent->right = temp;
     }
 
-    //Pointing necessary pointers to its corresponding nodes
-    temp->parent = (*rootptr)->parent; 
-    if(temp->left!=NULL) temp->left->parent = (*rootptr); 
-    (*rootptr)->right = temp->left; 
-    temp->left = (*rootptr);
-    (*rootptr)->parent = temp;
-    (*rootptr) = temp;
+    // Pointing necessary pointers to its corresponding nodes
+    temp -> parent = base -> parent;
+
+    if (temp -> left != NULL) temp -> left -> parent = base;
+
+    base -> right = temp -> left; 
+    temp -> left = base;
+    base -> parent = temp;
     
-    //Updates height of temp->left and temp
-    updateHeight(temp->left);
+    *root = temp;
+    
+    // Updates height of temp->left and temp
+    updateHeight(temp -> left);
     updateHeight(temp);
   }
 }
 
-//rightRotate() does a single left rotation given a root
-void rightRotate(WORD **rootptr){
+void rightRotate(WORD **root) {
+  WORD *base = *root;
+  WORD *temp = (*root) -> left; // Right rotation takes note of the left pointer of the root
 
-  WORD *temp = (*rootptr)->left;//Right rotation takes note of the left pointer of the root
-
-  if(rootptr!=NULL && *rootptr!=NULL && (*rootptr)->left!=NULL){
-  
-    
-    //Checks if the rootptr has a parent
-    if((*rootptr)->parent != NULL) {
-      if((*rootptr)->parent->right == (*rootptr)){ //This assign root->parent->left/right to temp
-        (*rootptr)->parent->right = temp;
-        (*rootptr) = temp->parent;
+  if (root != NULL && base != NULL && base -> left != NULL) {
+    // Checks if the root has a parent
+    if (base -> parent != NULL) {
+      if (base -> parent -> right == base) {
+        // Assigns root->parent->left/right to temp
+        base -> parent -> right = temp;
+        *root = temp -> parent;
+      } else {
+        base -> parent -> left = temp;
       }
-      else(*rootptr)->parent->left = temp;
     }
 
+    // Pointing necessary pointers to its corresponding nodes
+    temp -> parent = base -> parent;
 
-    //Pointing necessary pointers to its corresponding nodes
-    temp->parent = (*rootptr)->parent; 
-    if(temp->right!=NULL){
-      (*rootptr)->left = temp->right;
-      temp->right->parent = (*rootptr);
-    }else (*rootptr)->left = NULL;
-    (*rootptr)->left = temp->right;
-    temp->right = (*rootptr);
-    (*rootptr)->parent = temp;
-    (*rootptr) = temp; //New root would be the temp
+    if (temp -> right != NULL) {
+      base -> left = temp -> right;
+      temp -> right -> parent = base;
+    } else {
+      base -> left = NULL;
+    }
+
+    base -> left = temp -> right;
+    temp -> right = base;
+    base -> parent = temp;
+    *root = temp; // New root would be the temp
     
-
-    //Updates height of temp->left and temp
-    updateHeight(temp->right);
+    updateHeight(temp -> right);
     updateHeight(temp);
-
-
   }
 }
 
-void insertFixUp(WORD **rootptr, WORD *temp){
-
-  int balance_factor; //Notes the balance factor of a node
+void insertFixUp(WORD **root, WORD *temp) {
+  int balanceFactor;
 
   do{
-    balance_factor = checkBalance(temp); //Balance factor of a node
-    //Note that positive number would mean leaning left, whilst negative would mean leaning right
+    // Positive number would mean leaning left,
+    // whilst negative would mean leaning right
+    balanceFactor = checkBalance(temp);
 
+    if (balanceFactor > 0) {
+      // Left Leaning
+      balanceFactor = checkBalance(temp -> left); 
 
-    //Left leaning 
-    if(balance_factor == 2){ 
-      //Gets balance factor of the left subtree
-      balance_factor = checkBalance(temp->left); 
-
-      //If balance_factor is negative, it would mean leaning to the Right so Left-Right leaning
-      if(balance_factor < 0){ 
-        leftRotate(&(temp->left));
-        rightRotate(&(temp));
-      }
-      //Else positive, it would mean leaning to the Left so Left-Left leaning 
-      else{
+      if (balanceFactor < 0) {
+        leftRotate(&(temp -> left));
+        rightRotate(&temp);
+      } else {
         rightRotate(&temp);
       }
-    }
+    } else if (balanceFactor < 0) {
+      // Right Leaning
+      balanceFactor = checkBalance(temp -> right);
 
-    //Right leaning
-    else if (balance_factor == -2){ 
-      //Gets balance factor of the right subtree
-      balance_factor = checkBalance(temp->right);
-
-      //If balance_factor is negative, it would mean leaning to the left so Right-Left leaning
-      if(balance_factor > 0){
-        rightRotate(&(temp->right)); 
-        leftRotate(&(temp));
-      }
-
-      //Else positive, it would mean leaning to the right so Right-Right leaning
-      else{ 
+      if (balanceFactor > 0) {
+        rightRotate(&(temp -> right)); 
+        leftRotate(&temp);
+      } else {
         leftRotate(&temp);
       }
     }
 
-
     updateHeight(temp);
 
-    if(temp->parent==NULL)
-      *rootptr = temp;
-
-    temp = temp->parent;
-
-  } while(temp!=NULL);
+    if (temp -> parent == NULL) *root = temp;
+    temp = temp -> parent;
+  } while (temp != NULL);
 }
 
 void insertNode(WORD** root, WORD* temp) {
   WORD *base = *root;
   
-  if(*root==NULL) *root = temp;
-  else{
-    temp->parent = *root;
+  if (base == NULL) *root = temp;
+  else {
+    temp -> parent = base;
 
-    if(!cmpStr(base -> string, temp -> string))
-      insertNode(&((*root)->right),temp);
-    else
-      insertNode(&((*root)->left),temp);
+    if (!cmpStr(base -> string, temp -> string)) {
+      insertNode(&(base -> right), temp);
+    } else {
+      insertNode(&(base -> left), temp);
+    }
   }
 }
 
-void insertValue(WORD **rootptr, char* string){
-  WORD *temp;
-  temp = createWord(string);
-  insertNode(rootptr, temp);
-  insertFixUp(rootptr, temp);
+void insertValue(WORD **root, char* string) {
+  WORD *temp = createWord(string);
+
+  insertNode(root, temp);
+  insertFixUp(root, temp);
 }
 
 void viewTree(WORD* root, int tabs) {
@@ -203,12 +195,14 @@ void viewTree(WORD* root, int tabs) {
 }
 
 
-void deleteTree(WORD ** root){
+void deleteTree(WORD **root) {
+  WORD *base = *root;
 
-  if((*root) != NULL){
-    deleteTree(&((*root)->left));
-    deleteTree(&((*root)->right));
-    free(*root);
+  if (base != NULL) {
+    deleteTree(&(base -> left));
+    deleteTree(&(base -> right));
+    free(base);
+
     *root = NULL;
   }
 }
