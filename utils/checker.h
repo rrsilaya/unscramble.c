8a33de3618c80aad8925730d5d43d53f082ff207
cortezsydney <cbcortez3@up.edu.ph>
2018-11-15 21:10:21 +0800

    Fix spacing

--- utils/checker.h
+++ utils/checker.h
@@ -1,34 +1,33 @@
 void searchFile(char* string){
-    FILE *stream;
-    char *line = NULL;
-	size_t len = 0;
-    ssize_t read;
-    stream = fopen("./dictionary/words.txt", "r");
-
-    if (stream == NULL){
-	    printf("words.txt not in the directory");
-        exit(EXIT_FAILURE);
+  FILE *stream;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  stream = fopen("./dictionary/words.txt", "r");
+  if (stream == NULL){
+    printf("words.txt not in the directory");
+      exit(EXIT_FAILURE);
+  }
+  else{
+    while ((read = getline(&line, &len, stream)) != -1) {
+      if(strcmp(line, string) == 0){
+        printf("%s found inside dictionary %s\n", string, line);
+        break;
+      }else if(strcmp(line, string) > 0){
+        printf("%s-> NOT FOUND\n", string);
+        break;
+      }
     }
-    else{
-        while ((read = getline(&line, &len, stream)) != -1) {
-            if(strcmp(line, string) == 0){
-                printf("%s found inside dictionary %s\n", string, line);
-                break;
-            }else if(strcmp(line, string) > 0){
-                printf("%s-> NOT FOUND\n", string);
-                break;
-            }
-        }
-        free(line);
-        fclose(stream);
+    free(line);
+    fclose(stream);
   }
 }
 
 void searchDict(WORD* root) {
-    if (root != NULL) {
-        searchDict(root -> left);
-        searchFile(strcat(root->string, "\n"));
-        searchDict(root -> right);
-    }
+  if (root != NULL) {
+    searchDict(root -> left);
+    searchFile(strcat(root->string, "\n"));
+    searchDict(root -> right);
+  }
 }
 
