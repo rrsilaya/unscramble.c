741315ffe061c3ad543ab2f7540dc096f261280c
rrsilaya <rrsilaya@gmail.com>
2018-11-15 17:26:08 +0800

    Implement letter masking

--- utils/misc.h
+++ utils/misc.h
@@ -1,3 +1,6 @@
+#define ALPHABET 26
+#define ASCII_OFFSET 97
+
 int getWildcards(char *mask) {
   int counter = 0;
 
@@ -6,4 +9,60 @@ int getWildcards(char *mask) {
   }
 
   return counter;
+}
+
+char* getChoices(char *mask, char *letters, int wildcards) {
+  char *string = (char *) malloc(sizeof(char) * (wildcards + 1));
+  int len = 0;
+  
+  // @TODO: If you can think of other ways to check this, change this
+  // implementation.
+  int mask_counter[ALPHABET];
+  int letters_counter[ALPHABET];
+
+  // Initialize counters
+  for (int i = 0; i < ALPHABET; i++) {
+    mask_counter[i] = 0;
+    letters_counter[i] = 0;
+  }
+
+  for (int i = 0; i < strlen(mask); i++) {
+    if (mask[i] != '_') mask_counter[mask[i] - ASCII_OFFSET]++;
+  }
+
+  for (int i = 0; i < strlen(letters); i++) letters_counter[letters[i] - ASCII_OFFSET]++;
+
+  // Perform XOR operation
+  for (int i = 0; i < ALPHABET; i++) {
+    int difference = letters_counter[i] - mask_counter[i];
+
+    if (difference != 0) {
+      for (int mult = 0; mult < difference; mult++) {
+        string[len] = i + ASCII_OFFSET;
+        len++;
+      }
+    }
+
+    // @TODO: If difference < 0, this is an error.
+  }
+
+  string[wildcards] = '\0';
+  return string;
+}
+
+char* replaceWildcards(char *mask, char *letters) {
+  int index = 0, len = strlen(mask);
+  char *string = (char *) malloc(sizeof(char) * (len + 1));
+
+  for (int i = 0; i < len; i++) {
+    if (mask[i] == '_') {
+      string[i] = letters[index];
+      index++;
+    } else {
+      string[i] = mask[i];
+    }
+  }
+
+  string[len] = '\0';
+  return string;
 }
--- utils/permutation.h
+++ utils/permutation.h
@@ -1,10 +1,12 @@
 void permutation(char *letters, char *mask, WORD **root) {
   int len = strlen(letters);
-  int wildcard = getWildcards(mask);
 
-  char *nopts = (char *) malloc(sizeof(char) * (len + 2));
-  char **option = (char **) malloc(sizeof(char*) * (len + 2));
-  for (int i = 0; i < len + 2; i++) option[i] = (char *) malloc(sizeof(char) * (len + 2));
+  int wildcards = getWildcards(mask);
+  char *choices = getChoices(mask, letters, wildcards);
+
+  char *nopts = (char *) malloc(sizeof(char) * (wildcards + 2));
+  char **option = (char **) malloc(sizeof(char*) * (wildcards + 2));
+  for (int i = 0; i < wildcards + 2; i++) option[i] = (char *) malloc(sizeof(char) * (wildcards + 2));
 
   int start, move, i, candidate;
 
@@ -16,17 +18,17 @@ void permutation(char *letters, char *mask, WORD **root) {
       nopts[++move] = 0;
 
       // Print a solution
-      if (move - 1 == wildcard) {
+      if (move - 1 == wildcards) {
         char *solution = (char *) malloc(sizeof(char) * move);
 
-        for (i = 1; i < move; i++) solution[i - 1] = letters[option[i][nopts[i]]];
+        for (i = 1; i < move; i++) solution[i - 1] = choices[option[i][nopts[i]]];
         solution[move - 1] = '\0';
 
-        insertValue(root, solution);
+        insertValue(root, replaceWildcards(mask, solution));
       }
 
       // Find Candidates
-      for (candidate = len - 1; candidate >= 0; candidate--) {
+      for (candidate = wildcards - 1; candidate >= 0; candidate--) {
         for (i = move - 1; i > 0; i--) {
           if (option[i][nopts[i]] == candidate) break;
         }
