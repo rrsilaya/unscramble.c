7da996b67701e1c7a3fdcb4d3d776753edc97044
rrsilaya <rrsilaya@gmail.com>
2018-11-22 15:46:13 +0800

    Restructure project

---
+++ bin/unscramble.c
@@ -0,0 +1,28 @@
+#define MAX_WORD_SIZE 20
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "utils/misc.h"
+#include "utils/avl.h"
+#include "utils/permutation.h"
+#include "utils/checker.h"
+
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    printf("Wrong input: ./unscramble.exe <characters> <template>\n");
+
+    return 1;
+  } else {
+    WORD *solutions = NULL;
+    WORD *dictionary = NULL;
+
+    permutation(toLowerCase(argv[1]), toLowerCase(argv[2]), &solutions);
+    // viewTree(solutions, 0);
+    loadFile(&dictionary);
+    searchDict(solutions, dictionary);
+    return 0;
+  }
+}
