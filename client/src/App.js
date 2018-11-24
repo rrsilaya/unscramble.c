e61faf570015fff34aa5babb49032825af1e7834
cortezsydney <cbcortez3@up.edu.ph>
2018-11-25 03:33:33 +0800

    Initialize user interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  style = {
+    border: '3px solid green',
+    padding: '10px',
+    margin: 'auto',
+    color: '#7C0A02'
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <div className="card" style={{...this.style}}>
+            <img className="card-img-top" src="..." alt="*found words here*"/>
+            <div className="card-body" style={{width: '100%'}}>
+              <p className="card-text">words words words words words words words</p>
+            </div>
+            <div className="form-row">
+              <div className="col">
+                <input type="text" className="form-control" placeholder="Characters"/>
+              </div>
+              <div className="col">
+                <input type="text" className="form-control" placeholder="Mask"/>
+              </div>
+              <button type="button" className="btn btn-outline-success">Solve</button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
